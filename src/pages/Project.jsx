9448205829bcc663b395c06ec90c8996d5c7dc19
Massa Timothée <timothee.massa.perso@gmail.com>
2024-03-11 19:39:41 +0100

    add button to link project with repo

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -11,13 +11,18 @@ export default function Project() {
         setProject(projects[projectId - 1])
     }, [projectId])
 
+    function handleButtonClick() {
+        window.open(project.link, '_blank')
+    }
+
     return (
         <section className='project--container'>
             <h2>{project.name}</h2>
             <div className='project--top'>
-                <img src={project.image}/>
+                <img src={project.image} alt='project screenshot'/>
                 <p>{project.description}</p>
             </div>
+            <button onClick={handleButtonClick}>Visit Github repository</button>
         </section>
     );
 }
